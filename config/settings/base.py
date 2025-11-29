05398466dc8996d79f077fe8040560aaa6cc7d1f
stennn <stennn@msn.com>
2025-11-29 13:28:07 +0200

    create and config local apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,9 +41,9 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    # "core_apps.user_auth",
-    # "core_apps.common",
-    # "core_apps.user_profile",
+    "core_apps.user_auth",
+    "core_apps.common",
+    "core_apps.user_profile",
     # "core_apps.accounts",
     # "core_apps.cards",
 ]
