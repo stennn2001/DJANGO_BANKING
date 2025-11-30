593a80fe8da9b5186f8292d9f6499896c0ae9799
stennn <stennn@msn.com>
2025-11-30 14:00:27 +0200

    djago logging with  loguru

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+# from core_apps.user_auth.views import TestLoggingView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    # path("", TestLoggingView.as_view(), name="home"),
 ]
 # python -c "import secrets; print(secrets.token_urlsafe(38))"
--- core_apps/user_auth/views.py
+++ core_apps/user_auth/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from loguru import logger
 
-# Create your views here.
+# class TestLoggingView(View):
+#     def get(self, request):
+#         logger.debug("This is a debug message")
+#         logger.info("This is an info message")
+#         logger.warning("This is a warning message")
+#         logger.error("This is an error message")
+#         logger.critical("This is a critical message")
+#         return JsonResponse({"message": "We are testing logging with loguru"})
