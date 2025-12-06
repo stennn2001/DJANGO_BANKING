51247108f057519ad82aed2bb4c57fedf85f31e7
stennn <stennn@msn.com>
2025-12-06 14:20:53 +0200

    email OTP

--- config/settings/local.py
+++ config/settings/local.py
@@ -27,3 +27,16 @@ DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
 DOMAIN = getenv("DOMAIN")
 
 MAX_UPLOAD_SIZE = 1 * 1024 * 1024
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+]
+
+LOCKOUT_DURATION = timedelta(minutes=1)
+
+LOGIN_ATTEMPTS = 3
+
+OTP_EXPIRATION = timedelta(minutes=1)
+
